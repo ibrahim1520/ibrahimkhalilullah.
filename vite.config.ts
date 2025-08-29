6d7bb8e29d5ed93f87cc89dd5c78674fb41afb31
ibrahim khalilullah <68064164+ibrahim1520@users.noreply.github.com>
2025-08-30 00:28:19 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ibrahimkhalilullah/", // 👈 your repo name here
+  base: "/ibrahimkhalilullah./", // 👈 your repo name here
 })
